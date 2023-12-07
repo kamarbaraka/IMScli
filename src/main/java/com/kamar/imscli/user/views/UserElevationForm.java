e188bb4e1d1110c13fbc58968d05ed1c56e6b007
kamarbaraka <kamar254baraka@gmail.com>
2023-12-07 12:19:33 +0300

    implementing ticket referral form.
    - made a skeleton of the form.
    
    Signed-off-by: kamarbaraka <kamar254baraka@gmail.com>

--- src/main/java/com/kamar/imscli/user/views/UserElevationForm.java
+++ src/main/java/com/kamar/imscli/user/views/UserElevationForm.java
@@ -1,5 +1,9 @@
 package com.kamar.imscli.user.views;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -11,6 +15,35 @@ import com.vaadin.flow.router.Route;
 public class UserElevationForm extends VerticalLayout {
 
 
+    private final ComboBox<String > roleField;
+    private final ComboBox<String> userField;
+    private final Checkbox elevateField;
+    private final Button submitButton;
+    private final HorizontalLayout titleBar;
+    private final HorizontalLayout contentLayout;
+
+
     public UserElevationForm() {
+        /*inject dependencies*/
+
+        /*initialize the components*/
+        this.roleField = new ComboBox<>();
+        this.elevateField = new Checkbox();
+        this.userField = new ComboBox<>();
+        this.submitButton = new Button();
+        this.titleBar = new HorizontalLayout();
+        this.contentLayout = new HorizontalLayout();
+
+        /*configure layout*/
+        configureLayout();
+    }
+
+    private void configureLayout() {
+
+        configureRoleField();
+    }
+
+    private void configureRoleField() {
+
     }
 }
