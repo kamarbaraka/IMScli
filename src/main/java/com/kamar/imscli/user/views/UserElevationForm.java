167faaeb0f3b26fa1b232fecd993cc759a77a3bf
kamarbaraka <kamar254baraka@gmail.com>
2023-12-07 11:58:12 +0300

    implemented the form for an employee to refer a ticket.
    Signed-off-by: kamarbaraka <kamar254baraka@gmail.com>

---
+++ src/main/java/com/kamar/imscli/user/views/UserElevationForm.java
@@ -0,0 +1,16 @@
+package com.kamar.imscli.user.views;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+/**
+ * form to elevate a user.
+ * @author kamar baraka.*/
+
+@Route("user_elevation")
+public class UserElevationForm extends VerticalLayout {
+
+
+    public UserElevationForm() {
+    }
+}
