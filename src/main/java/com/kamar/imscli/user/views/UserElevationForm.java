e5da786dd357125af5b8926c130b055fc84573a8
kamarbaraka <kamar254baraka@gmail.com>
2023-12-09 18:19:37 +0300

    implemented the form to elevate or demote a user.
    Signed-off-by: kamarbaraka <kamar254baraka@gmail.com>

--- src/main/java/com/kamar/imscli/user/views/UserElevationForm.java
+++ src/main/java/com/kamar/imscli/user/views/UserElevationForm.java
@@ -3,6 +3,9 @@ package com.kamar.imscli.user.views;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -18,7 +21,7 @@ public class UserElevationForm extends VerticalLayout {
     private final ComboBox<String > roleField;
     private final ComboBox<String> userField;
     private final Checkbox elevateField;
-    private final Button submitButton;
+    private final Button elevateButton;
     private final HorizontalLayout titleBar;
     private final HorizontalLayout contentLayout;
 
@@ -30,7 +33,7 @@ public class UserElevationForm extends VerticalLayout {
         this.roleField = new ComboBox<>();
         this.elevateField = new Checkbox();
         this.userField = new ComboBox<>();
-        this.submitButton = new Button();
+        this.elevateButton = new Button();
         this.titleBar = new HorizontalLayout();
         this.contentLayout = new HorizontalLayout();
 
@@ -41,9 +44,108 @@ public class UserElevationForm extends VerticalLayout {
     private void configureLayout() {
 
         configureRoleField();
+        configureElevateField();
+        configureUserField();
+        configureElevateButton();
+
+        initListeners();
+
+        this.add(roleField, userField, elevateField, elevateButton);
+        this.setAlignItems(Alignment.CENTER);
     }
 
     private void configureRoleField() {
+        /*configure the role field*/
+        roleField.setLabel("role to elevate/demote");
+        roleField.setRequired(true);
+        roleField.setRequiredIndicatorVisible(true);
+        roleField.setAutofocus(true);
+        roleField.setClearButtonVisible(true);
+        roleField.setMinWidth("500px");
+        roleField.setErrorMessage("provide a role");
+        roleField.setPlaceholder("select a role");
+        roleField.setItems("role 1", "role2", "role3", "role 4");
 
     }
+
+    private void configureUserField() {
+        /*configure the user field*/
+        userField.setLabel("user to elevate/demote");
+        userField.setEnabled(false);
+        userField.setRequired(true);
+        userField.setRequiredIndicatorVisible(true);
+        userField.setClearButtonVisible(true);
+        userField.setMinWidth("500px");
+        userField.setErrorMessage("provide a user");
+        userField.setPlaceholder("select a user");
+        userField.setItems("user 1", "user 2", "user 3", "user 4");
+
+    }
+
+    private void configureElevateField() {
+        /*configure the elevated checkbox*/
+        elevateField.setLabel("demote");
+        elevateField.setEnabled(false);
+        elevateField.setTooltipText("check to demote, uncheck to elevate");
+
+    }
+
+    private void configureElevateButton() {
+        /*configure the elevate button*/
+        elevateButton.setText("elevate");
+        elevateButton.setEnabled(false);
+        elevateButton.setIcon(new Icon(VaadinIcon.ARROW_CIRCLE_UP));
+        elevateButton.setMinWidth("300px");
+
+    }
+
+    private void initListeners() {
+        /*configure listeners*/
+        roleField.addValueChangeListener(event -> {
+            userField.setEnabled(true);
+        });
+
+        userField.addValueChangeListener(event -> {
+            elevateField.setEnabled(true);
+            elevateField.setLabel("demote");
+            elevateButton.getStyle().setBackground("green");
+            elevateButton.getStyle().setColor("white");
+            elevateButton.setText("elevate");
+            elevateButton.setIcon(new Icon(VaadinIcon.ARROW_CIRCLE_UP));
+            elevateButton.setEnabled(true);
+
+        });
+
+        elevateField.addValueChangeListener(event -> {
+
+            if (elevateField.getValue()) {
+                elevateField.setLabel("elevate");
+                elevateButton.getStyle().setBackground("red");
+                elevateButton.getStyle().setColor("white");
+                elevateButton.setText("demote");
+                elevateButton.setIcon(new Icon(VaadinIcon.ARROW_CIRCLE_DOWN));
+                elevateButton.setEnabled(true);
+            }else {
+                elevateField.setLabel("demote");
+                elevateButton.getStyle().setBackground("green");
+                elevateButton.getStyle().setColor("white");
+                elevateButton.setText("elevate");
+                elevateButton.setIcon(new Icon(VaadinIcon.ARROW_CIRCLE_UP));
+                elevateButton.setEnabled(true);
+            }
+
+        });
+
+        elevateButton.addClickListener(event -> {
+            if (roleField.isEmpty() || userField.isEmpty()) {
+                Notification.show("fill all required inputs");
+                return;
+            }
+            Notification.show("clicked");
+        });
+
+    }
+
+
+
 }
